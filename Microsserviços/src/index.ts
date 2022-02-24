465b699d6950150a78a1cd7e7ccb09320aa5fabb
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-02-24 15:00:49 -0300

    Implementado métodos put e delete, alterado rota de status, projeto finalizado

--- Microsserviços/src/index.ts
+++ Microsserviços/src/index.ts
@@ -1,13 +1,20 @@
 import express, {Request, Response, NextFunction} from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
+
+// Config. da aplicação 
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+
+// Config. das rotas
 app.use(usersRoute);
 
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: "bar"});
-});
+// Config. rota de status
+app.use(statusRoute);
 
+// Inicialização do servidor na porta determinada
 app.listen(3000, () => {
     console.log("Aplicação online na porta 3000");
 });
--- Microsserviços/src/routes/users.route.ts
+++ Microsserviços/src/routes/users.route.ts
@@ -1,16 +1,39 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { request } from "http";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
+// Read all
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
     const users = [{name: "Thiago"}];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
+// Read only
 usersRoute.get("/users/:uuid", (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({uuid});
+    res.status(StatusCodes.OK).send({uuid});
 });
+
+// Create
+usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+// Update
+usersRoute.put("users/:uuid", (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const updatedUser = req.body;
+    updatedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send(updatedUser);
+});
+
+// Delete
+usersRoute.delete("users/:uuid", (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.OK);
+})
 
 export default usersRoute;
