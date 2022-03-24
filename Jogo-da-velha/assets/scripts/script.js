5eb32e6f539e2c1c8726094f73066339c46f1827
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-03-24 17:13:07 -0300

    :construction: Commit inicial para o script js

--- Jogo-da-velha/assets/scripts/script.js
+++ Jogo-da-velha/assets/scripts/script.js
@@ -1,5 +1,15 @@
-let jogadorAtual = null
+var jogador = null
+var jogadorAtual = document.getElementById("jogadorAtual")
+
+trocaJogador('X')
 
 function escolhendoQuadrado(id){
-    console.log(`Este é o id de número ${id}`)
+    var quadrado = document.getElementById(id)
+    quadrado.innerHTML = jogador
+    console.log(id)
+}
+
+function trocaJogador(valor){
+    jogador = valor
+    jogadorAtual.innerHTML = jogador
 }
