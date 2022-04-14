f41d82c981aca6e6c02065508fe3d2537117aa6f
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-04-14 12:17:13 -0300

    Commit inicial, primeiros passos com .NET

---
+++ C#Learning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ntimes = 5;
+
+            for(int i = 0; i < ntimes; i++){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
